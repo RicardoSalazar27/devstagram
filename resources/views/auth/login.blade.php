a7f32a16fb06e654d9e7dc4b98b5f96e9a71d6cd
ricardosexenes <edgar_ss2012@hotmail.com>
2026-03-23 20:55:24 -0600

    terminamos clase 85

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,8 +11,11 @@
         </div>
 
         <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
-            <form novalidate>
+            <form method="POST" action="{{ route('login') }}" novalidate>
                 @csrf
+                @if (session('mensaje'))
+                 <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ session('mensaje') }}</p>
+                @endif
                 <div class="mb-5">
                     <label for="email" class="mb-2 block uppercase text-gray-500 font-bold" for="email">Email</label>
                     <input 
@@ -42,6 +45,12 @@
                         <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
                     @enderror 
                 </div>
+
+                <div class="mb-5">
+                    <input type="checkbox" name="remember" id="remember">
+                    <label class="text-gray-500 text-sm" for="remember">Mantener mi sesión abierta</label>
+                </div>
+
                 <input 
                     type="submit" 
                     value="Iniciar Sesión" 
