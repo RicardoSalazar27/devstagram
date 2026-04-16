215573a845e906c87d4b900237e93515a0ae9771
ricardosexenes <edgar_ss2012@hotmail.com>
2026-04-15 21:45:04 -0600

    termianmos clase 100 para empezar clase 101 de factories

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -4,26 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Intervention\Image\Laravel\Facades\Image;
 
 class ImagenController extends Controller
 {
-    //
     public function store(Request $request)
     {
-        // return "desde imagen controller";
-        // $input = $request->all();
-        
-        // return response()->json($input);
+        $request->validate([
+            'file' => 'required|image|max:5120',
+        ]);
 
         $imagen = $request->file('file');
-
         $nombreImagen = Str::uuid() . '.' . $imagen->extension();
 
-        /*
-        Comprobamos que estamos obteniendo bien la imagen
-        return response()->json(['imagen' => $imagen->extension() ]);
-        */
+        // Aquí va decode(), no make() ni read()
+        $imagenServidor = Image::decode($imagen);
 
-        return response()->json(['imagen' => $nombreImagen ]);
+        // Redimensionar / recortar
+        $imagenServidor->cover(1000, 1000);
+
+        // Ruta destino
+        $imagenPath = public_path('uploads/' . $nombreImagen);
+
+        // Guardar
+        $imagenServidor->save($imagenPath);
+
+        return response()->json([
+            'imagen' => $nombreImagen
+        ]);
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -12,3 +12,20 @@ const dropzone = new Dropzone('#dropzone', {
     maxFiles: 1,
     uploadMultiple: false
 });
+
+dropzone.on('sending', function(file, xhr, formData) {
+    console.log(file);//vemos la informacion del archivo
+    console.log(formData);//vemos la informacion del formulario
+})
+
+dropzone.on('success', function(file, response) {
+    console.log(response);//vemos la respuesta del servidor
+})
+
+dropzone.on('error', function(file, message) {
+    console.log(message);//vemos la respuesta del servidor si falla la subida
+})
+
+dropzone.on('removedfile', function(){
+    console.log('archivo eliminado');
+})
