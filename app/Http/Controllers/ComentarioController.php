b0546fd32ea16b8db1f2da89f58e0e44421b9909
ricardosexenes <edgar_ss2012@hotmail.com>
2026-05-07 23:12:46 -0600

    terminamos clase 125

---
+++ app/Http/Controllers/ComentarioController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comentario;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ComentarioController extends Controller
+{
+    //
+
+    public function store(Request $request, User $user, Post $post){
+        // dd('comentando...');
+        
+        //El user que recibe store es el username del muro en el que estamos
+        //comentando, no el usuario autenticado
+        
+        // Validar
+        $this->validate($request, [
+            'comentario' => 'required|max:255'
+        ]);
+
+        // Almacenar comentario
+        Comentario::create([
+            'user_id' => auth()->user()->id,
+            'post_id' => $post->id,
+            'comentario' => $request->comentario
+        ]);
+
+        // Imprimir un mensaje
+        return back()->with('mensaje', 'Comentario Realizado Correctamente');
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ComentarioController;
 use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
@@ -34,3 +35,5 @@ Route::get('/post/create', [PostController::class, 'create'])->name('posts.creat
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 Route::post('/imagenes', [ImagenController::class, 'store'])->name('imagenes.store');
 Route::get('/{user:username}/posts/{post}', [PostController::class, 'show']) ->name('posts.show');
+
+Route::post('/{user:username}/posts/{post}', [ComentarioController::class, 'store']) ->name('comentarios.store');
