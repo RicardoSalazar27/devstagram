c89da4524d4aa5e94bc8370301fc601646c73ed7
ricardosexenes <edgar_ss2012@hotmail.com>
2026-08-04 15:26:51 -0600

    terminamos clase 161

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -60,22 +60,6 @@
         {{-- por medio de un for each recorremos las publicaciones/post del usuario, que mandamos
         desde el controlador, mostrando cada imagen del post y cada imagen con un enlace.
         si no hay post, mostramos un mensjae de que no hay post --}}
-        @if ($posts->count())
-            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                @foreach ($posts as $post)
-                    <div>
-                        <a href="{{ route('posts.show', ['post' => $post, 'user' => $post->user]) }}">
-                            <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo}} ">
-                        </a>
-                    </div>
-                @endforeach
-            </div>
-            <div class="my-10">
-                {{-- aqui van los enlaces de paginacion --}}
-                {{ $posts->links('pagination::tailwind') }}
-            </div>
-        @else
-            <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay posts.</p>
-        @endif
+        <x-listar-post :posts="$posts" />
     </section>
 @endsection
